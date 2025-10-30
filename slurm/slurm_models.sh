6ddcb5ea275b92720d0f41e7660a8daac0dd0dd1
jarlsoeren <jarlsoeren@gmail.com>
2025-10-30 09:58:50 +0100

    updates

--- slurm/slurm_models.sh
+++ slurm/slurm_models.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #SBATCH --partition=HPC
 #SBATCH -o /mnt/scratch/projects/rewardMap/logs/testing_models_%j.out
-#SBATCH -J lss
+#SBATCH -J models
 #SBATCH -n 1
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=30G
 
 
-PYTHON_BIN=/mnt/scratch/projects/rewardMap/reward_signature/venv/bin/python
+PYTHON_BIN=/mnt/projects/rewardMap/STUDIES/reward_signature/.venv/bin/python
 ANACONDA_DIR=/mnt/projects/rewardMap/STUDIES/preregistration/.venv/
 
 #DISPLAY=
 #. $ANACONDA_DIR/etc/profile.d/conda.sh
 
-COMPUTE_FOLDER='/mnt/scratch/projects/rewardMap/reward_signature/'
+COMPUTE_FOLDER='/mnt/projects/rewardMap/STUDIES/reward_signature/scripts'
 
 # activate the env your interested in
 
-$PYTHON_BIN $COMPUTE_FOLDER/call_testing_model.py
+$PYTHON_BIN $COMPUTE_FOLDER/reward_system_masking_model.py
